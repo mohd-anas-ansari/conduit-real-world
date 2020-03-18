402ebe5dc1067c898c445b2cd9f75b7a5cdce86f
Anas Ansari <anasansari157@gmail.com>
2020-03-18 21:34:05 +0530

    SignUp Working

--- src/components/login.js
+++ src/components/login.js
@@ -1,21 +1,60 @@
 import React from "react";
 
-const Login = () => {
+const Login = (props) => {
+	let email = React.useRef(null);
+	let password = React.useRef(null);
+	console.log("Email", email);
+
+	function handleLogin(e) {
+		e.preventDefault()
+		fetch("https://conduit.productionready.io/api/users/login ", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify({
+				user: {
+					email: email.current.value,
+					password: password.current.value
+				}
+			})
+		})
+			.then((res) => res.json())
+			.then((user) => {
+				if (user.errors) {
+					localStorage.setItem("isLoggedIn", false);
+				} else {
+					localStorage.setItem("isLoggedIn", true);
+					props.history.push("/");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
 	return (
 		<>
 			<div className="container width-50">
-
-			<h2 className="title margin-10 text-center">Login</h2>
+				<h2 className="title margin-10 text-center">Login</h2>
 				<form>
-					<input class="input is-primary margin-10" type="email" placeholder="Email" />
+					<input
+						class="input is-primary margin-10"
+						type="email"
+						placeholder="Email"
+						ref={email}
+					/>
 
 					<input
 						class="input is-primary margin-10"
 						type="password"
 						placeholder="Password"
+						ref={password}
 					/>
 
-					<button class="button is-success margin-10">Log In</button>
+					<button class="button is-success margin-10" onClick={(e) => { handleLogin(e) }}>
+						Log In
+					</button>
 				</form>
 			</div>
 		</>
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,29 @@
 import React from "react";
 
 const Signup = () => {
+	let username = React.useRef(null);
+	let password = React.useRef(null);
+	let email = React.useRef(null);
+
+	function handleSignup(e) {
+		e.preventDefault();
+		console.log(email.current.value);
+		fetch("https://conduit.productionready.io/api/users", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify({
+				user: {
+					username: username.current.value,
+					email: email.current.value,
+					password: password.current.value
+				}
+			})
+		})
+			.then((res) => res.json())
+			.then((userData) => console.log(userData));
+	}
 	return (
 		<>
 			<div className="container width-50">
@@ -10,21 +33,29 @@ const Signup = () => {
 						className="input is-primary margin-10"
 						type="text"
 						placeholder="Username"
+						ref={username}
 					/>
 
 					<input
 						className="input is-primary margin-10"
 						type="email"
 						placeholder="Email"
+						ref={email}
 					/>
 
 					<input
 						className="input is-primary margin-10"
 						type="password"
 						placeholder="Password"
+						ref={password}
 					/>
 
-					<button className="button is-success margin-10">Sign Up</button>
+					<button
+						className="button is-success margin-10"
+						onClick={(e) => handleSignup(e)}
+					>
+						Sign Up
+					</button>
 				</form>
 			</div>
 		</>
